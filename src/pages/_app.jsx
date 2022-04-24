2e2101ef2523989ad8b73fbddea3087304aef5b4
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-24 22:33:37 +0100

    feat(_app.jsx): dayjs config

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/pt';
 
 function MyApp({ Component, pageProps }) {
+  dayjs.extend(relativeTime);
+  dayjs.locale('pt');
+
   return <Component {...pageProps} />;
 }
 
