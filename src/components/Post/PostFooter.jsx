43091c135d6057c07fc83417c55028beca1dfd97
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-25 12:38:32 +0100

    refactor(postfooter.jsx): updated to only have border at the top

--- src/components/Post/PostFooter.jsx
+++ src/components/Post/PostFooter.jsx
@@ -2,7 +2,7 @@ import { HeartIcon, BookmarkIcon } from '@heroicons/react/outline';
 
 export default function PostFooter() {
   return (
-    <div className="mt-2 border-y flex justify-evenly items-center">
+    <div className="mt-2 border-t flex justify-evenly items-center">
       <button className="postInputBtn">
         <HeartIcon className="w-6 h-6" />
         <span className="text-sm">Gostar da publicação</span>
