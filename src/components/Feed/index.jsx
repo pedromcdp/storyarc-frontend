343b52e826ef4f32a4048aba1ed8895151146750
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-26 22:24:36 +0100

    refactor(feed): ajusted the breakpoints

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -3,13 +3,13 @@ import PostsContainer from './PostsContainer';
 
 export default function Feed() {
   return (
-    <div className="overflow-y-auto grow 2xl:flex 2xl:flex-row 2xl:justify-center 2xl:mx-auto 2xl:space-x-4 2xl:w-full">
-      <div className="grow mx-auto max-w-md h-screen md:max-w-lg lg:max-w-2xl 2xl:mx-0">
+    <div className="overflow-y-auto grow xl:flex xl:flex-row xl:justify-center xl:mx-auto xl:space-x-4 xl:w-full">
+      <div className="grow mx-auto max-w-md h-screen md:max-w-lg lg:max-w-2xl xl:mx-0">
         <FeedFilter />
         <PostsContainer />
       </div>
-      <div className="hidden top-5 grow mt-5 max-w-xs 2xl:block 2xl:sticky">
-        <div className="2xl:max-w-xs">
+      <div className="hidden top-5 grow mt-5 max-w-xs xl:block xl:sticky">
+        <div className="xl:max-w-xs">
           <div className="p-5 bg-verde rounded-xl shadow-sm">
             <h1 className="text-white">123</h1>
           </div>
