3f3f696f52255853e3bdcd494a8ef920cf8f5bc1
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-26 11:28:11 +0100

    chore(public): added assets

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,33 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#37b780" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="O storyarc é uma plataforma de arquivo de conteúdos que pressupõe a partilha e consulta de informações relativas a espaços e à sua evolução ao longo dos anos, de modo a preservar a memória patrimonial do distrito de Aveiro."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
