39f4f7af077292271f59d2b16028e59063af796b
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-27 16:13:32 +0100

    feat(redux): created redux store and added it to _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,18 @@ import '../styles/globals.css';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/pt';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 
 function MyApp({ Component, pageProps }) {
   dayjs.extend(relativeTime);
   dayjs.locale('pt');
 
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
