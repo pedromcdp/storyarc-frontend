9765fce26ba41098ae2df9f722cfe586caa49eee
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-04-29 15:40:35 +0100

    refactor(header): header
    changed div tag to a header tag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,10 @@ import HeaderRight from './HeaderRight';
 
 export default function Header() {
   return (
-    <div className="flex sticky top-0 z-30 justify-between items-center py-1 px-4 bg-white shadow-sm md:hidden">
+    <header className="flex sticky top-0 z-30 justify-between items-center py-1 px-4 bg-white shadow-sm md:hidden">
       <HeaderLeft />
       <HeaderCenter />
       <HeaderRight />
-    </div>
+    </header>
   );
 }
