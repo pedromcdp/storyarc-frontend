d331b6ea9c21245f7ad2b8547cce2add15694c85
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-05-11 12:23:01 +0100

    refactor(rowscontainer): added a prop so user can add more classes

--- src/components/Sidebar/RowsContainer.jsx
+++ src/components/Sidebar/RowsContainer.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import PropTypes from 'prop-types';
 
-export default function RowsContainer({ children, title, ariaText }) {
+export default function RowsContainer({
+  children,
+  title,
+  ariaText,
+  moreClasses,
+}) {
   return (
-    <section className="md:mt-4 w-max-full">
+    <section className={`md:mt-4 w-max-full ${moreClasses}`}>
       <h1
         className="mb-1 text-sm tracking-wide text-gray-500 md:text-base"
         tabIndex={0}
@@ -22,4 +27,5 @@ RowsContainer.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
   ariaText: PropTypes.string.isRequired,
+  moreClasses: PropTypes.string,
 };
