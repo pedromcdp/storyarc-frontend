9b57b7f877e35c3b4c69211dbd8787774964e8e0
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-05-17 22:51:55 +0100

    feat(postfooter): added liked and bookmarked

--- src/components/Post/PostFooter.jsx
+++ src/components/Post/PostFooter.jsx
@@ -1,10 +1,26 @@
 import { HeartIcon, BookmarkIcon } from '@heroicons/react/outline';
+import {
+  HeartIcon as SolidHeartIcon,
+  BookmarkIcon as SolidBookmarkIcon,
+} from '@heroicons/react/solid';
+import { useState } from 'react';
 
 export default function PostFooter({
   showComments,
   setShowComments,
   currentUser,
 }) {
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
+  const handleBookmark = () => {
+    setBookmarked(!bookmarked);
+  };
+
   return (
     <>
       <div className="flex justify-end my-1">
@@ -17,13 +33,37 @@ export default function PostFooter({
       </div>
       {currentUser && (
         <div className="flex justify-evenly items-center border-t">
-          <button className="postInputBtn">
-            <HeartIcon className="w-6 h-6" />
-            <span className="text-sm">Gostar da publicação</span>
+          <button
+            className="transition-all duration-75 ease-in-out postInputBtn"
+            onClick={handleLike}
+          >
+            {liked ? (
+              <>
+                <SolidHeartIcon className="w-6 h-6 text-verde" />
+                <span className="text-sm">Liked</span>
+              </>
+            ) : (
+              <>
+                <HeartIcon className="w-6 h-6" />
+                <span className="text-sm">Gostar da publicação</span>
+              </>
+            )}
           </button>
-          <button className="postInputBtn">
-            <BookmarkIcon className="w-6 h-6" />
-            <span className="text-sm">Guardar publicação</span>
+          <button
+            className="transition-all duration-75 ease-in-out postInputBtn"
+            onClick={handleBookmark}
+          >
+            {bookmarked ? (
+              <>
+                <SolidBookmarkIcon className="w-6 h-6 text-verde" />
+                <span className="text-sm">Publicação Guardada</span>
+              </>
+            ) : (
+              <>
+                <BookmarkIcon className="w-6 h-6" />
+                <span className="text-sm">Guardar publicação</span>
+              </>
+            )}
           </button>
         </div>
       )}
