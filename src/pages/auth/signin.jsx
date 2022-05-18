05897114b35702e8b3d08a5956c4060b90f072e2
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-05-18 16:51:40 +0100

    feat(auth/login): renamed login page and changed the endpoint to /auth/signin

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Login from '../components/Login';
+import Login from '../../components/Login';
 
 export default function LoginPage() {
   return (
