56668da8a678da5300205af415185d8f18259b1e
joaoteixeira.pedro4 <joaoteixeira.pedro4@gmail.com>
2022-05-25 11:04:19 +0100

    feat(content slice): added content slice

---
+++ src/features/addContent/addContentSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  show: false,
+};
+
+const addContentSlice = createSlice({
+  name: 'content',
+  initialState,
+  reducers: {
+    setAddContent: (state, action) => {
+      state.show = action.payload;
+    },
+  },
+});
+
+export const { setAddContent } = addContentSlice.actions;
+export const isShowingContentModal = (state) => state.add.show;
+export default addContentSlice.reducer;
