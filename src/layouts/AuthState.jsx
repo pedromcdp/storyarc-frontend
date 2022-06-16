62924aa697503d6e0aaeb58c2f5eb6f345f55f2d
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-06-16 22:51:41 +0100

    refactor(authstate.jsx): implemented cron timer to revalidate user token

--- src/layouts/AuthState.jsx
+++ src/layouts/AuthState.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import PropTypes from 'prop-types';
+import cron from 'cron';
 import useAuth from '../hooks/auth';
 import { useAddUserMutation } from '../services/storyarc';
 import { SplashScreen } from '../components/SplashScreen';
@@ -11,6 +12,12 @@ export default function AuthState({ children }) {
   const { setUser, setToken } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
   const [addUser] = useAddUserMutation();
+  const [job] = useState(
+    new cron.CronJob('0 */15 * * * *', async () => {
+      const token = await firebase.auth().currentUser.getIdToken();
+      setToken(token);
+    }),
+  );
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(async (user) => {
@@ -27,6 +34,7 @@ export default function AuthState({ children }) {
       }
       setIsLoading(false);
     });
+    job.start();
   }, []);
 
   if (isLoading) {
