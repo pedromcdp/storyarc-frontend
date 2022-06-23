84212b3f7f8c6c699f444adc5d95c352ea92161f
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-06-23 11:53:37 +0100

    feat(skiptofeedbtn): added button to skip to feed content to improve accessibility

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -15,6 +15,7 @@ import {
 } from '../features/addContent/addContentSlice';
 import { useSearch } from '../features/search/searchSlice';
 import MobileSearch from '../components/Search/MobileSearch';
+import SkipToFeedBtn from '../components/SkipToFeedBtn';
 
 export default function MainLayout({ title, children }) {
   const isShowing = useSelector(useAddContent);
@@ -32,13 +33,17 @@ export default function MainLayout({ title, children }) {
       </Head>
       <Header />
       <main className="flex relative">
+        <SkipToFeedBtn />
         <Sidebar />
         {showMobileSearch && <MobileSearch />}
         <div
           id="scrollparent"
           className="overflow-y-auto grow scroll-smooth xl:flex xl:flex-row xl:justify-center xl:mx-auto xl:space-x-4 xl:w-full"
         >
-          <div className="grow mx-auto max-w-md h-screen md:max-w-lg lg:max-w-2xl xl:mx-0">
+          <div
+            id="feed"
+            className="grow mx-auto max-w-md h-screen md:max-w-lg lg:max-w-2xl xl:mx-0"
+          >
             {children}
           </div>
           <div className="hidden top-5 grow mt-5 max-w-xs xl:block xl:sticky">
