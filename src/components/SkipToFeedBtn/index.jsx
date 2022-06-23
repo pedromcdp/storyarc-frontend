58808d95f64579375fddab47f59707c1f2db55a7
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-06-23 13:55:53 +0100

    refactor(skiptofeedbtn): updated the margin left

--- src/components/SkipToFeedBtn/index.jsx
+++ src/components/SkipToFeedBtn/index.jsx
@@ -2,7 +2,7 @@ export default function SkipToFeedBtn() {
   return (
     <a
       href="#feed"
-      className="fixed left-0 z-50 items-center  py-3 px-5 mt-2 ml-8 bg-white rounded-xl border border-verde outline-verde drop-shadow-sm duration-200 -translate-x-96 focus:translate-x-0 transition-translate"
+      className="fixed left-0 z-50 items-center  py-3 px-5 mt-2 ml-2 bg-white rounded-xl outline-verde drop-shadow-sm duration-200 -translate-x-96 focus:translate-x-0 transition-translate"
     >
       Saltar para o Feed
     </a>
