74b456a19d1f7b5b09219ab13b458ea8be5ea815
Pedro Miguel Pereira <pedromcdp@gmail.com>
2023-05-16 16:43:58 +0100

    refactor(appurls.js): changed backend url

--- src/utils/appUrls.js
+++ src/utils/appUrls.js
@@ -6,7 +6,7 @@ const pageUrl =
 const apiUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'http://localhost:8080';
+    : 'https://storyarc-api-git-master-pmcdp.vercel.app';
 
 const defaultAvatarUrl =
   'https://firebasestorage.googleapis.com/v0/b/storyarc-bf600.appspot.com/o/avatars%2Favatar.webp?alt=media';
